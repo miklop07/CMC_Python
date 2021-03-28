9473f248acfff3059347936b896d71ad8e22b02c
miklop <miklop07@gmail.com>
2021-03-28 15:02:36 +0300

    main windows added

--- 05_SshAndSmartWidgents/main.py
+++ 05_SshAndSmartWidgents/main.py
@@ -7,7 +7,7 @@ class Application(tk.Frame):
         self.master.title(title)
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
-        self.master.geometry("800x600")
+        # self.master.geometry("800x600")
         self.grid(sticky="NEWS")
         self.create_widgets()
         for column in range(self.grid_size()[0]):
@@ -20,7 +20,11 @@ class Application(tk.Frame):
 
 class App(Application):
     def create_widgets(self):
-        pass
+        self.text = tk.Text(self, undo=True)
+        self.text.grid(row=0, column=0, sticky="NEWS")
+
+        self.graphics = tk.Canvas(self)
+        self.graphics.grid(row=0, column=1, sticky="NEWS")
 
     
 
