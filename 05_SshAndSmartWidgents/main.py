ddb3aa3268189f22c8220da0208014e22abb148b
miklop <miklop07@gmail.com>
2021-03-28 14:38:19 +0300

    05 lecture homework app init

---
+++ 05_SshAndSmartWidgents/main.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import inspect
+
+class Application(tk.Frame):
+    def __init__(self, master=None, title="<application>", **kwargs):
+        super().__init__(master, **kwargs)
+        self.master.title(title)
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.master.geometry("800x600")
+        self.grid(sticky="NEWS")
+        self.create_widgets()
+        for column in range(self.grid_size()[0]):
+            self.columnconfigure(column, weight=1)
+        for row in range(self.grid_size()[1]):
+            self.rowconfigure(row, weight=1)
+
+    def create_widgets(self):
+        pass
+
+class App(Application):
+    def create_widgets(self):
+        pass
+
+    
+
+app = App(title="Graph Edit")
+app.mainloop()
